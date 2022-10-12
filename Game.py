1dcc183078663f71e529af31b34e0f305740d4be
BHalpert <bhalpert@potomacschool.org>
2022-10-12 09:03:32 -0400

    Framework for Game Class with updated Instance Variables and Constructors with rough layout of StartTurn() Methid

--- Game.py
+++ Game.py
@@ -1,16 +1,31 @@
-class Player:
+import Gameboard
+import Human
+import Player
 
-    def __init__(self, name, playerNum):
 
-        self.name = name
-        self.playerNum = playerNum
+class Game:
+    def __init__(self, playerToMove, board, player0, player1):
+        self.playerToMove = playerToMove
+        self.board = board
+        self.player0 = player0
+        self.player1 = player1
+        self.Gameboard = Gameboard()
+        self.player0 = Player()
+        self.player1 = Player()
 
-    def getName(self):
 
-        return self.name
+    def getPlayerToMove(self):
+        return self.playerToMove
 
-    def getPlayerNum(self):
+    def getBoard(self):
+        return self.board
 
-        return self.playerNum
+    def getPlayer0(self):
+        return self.player0
 
-    
+    def getPlayer1(self):
+        return self.player1
+
+    def startTurn():
+        Move(index, direction, playerToMove)
+        IsGameOver()
