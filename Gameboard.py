c628bb2d35f762ee07aee0409a9137c4c74ce93e
cortmans2 <cortmans@potomacschool.org>
2022-10-14 08:47:42 -0400

    commit 3

--- Gameboard.py
+++ Gameboard.py
@@ -14,36 +14,46 @@ class Gameboard:
 
     def sow(self, index, direction, player):
 
-        current = count = self.gameboard[player][index]
+        count = self.gameboard[player][index]
+        current = [player, index]
         self.gameboard[player][index] = 0
 
         for i in range(count):
 
             if player == 0:
                 if direction == 0:
-                    if current < 0:
-                        current = self.gameboard[1][0]
+                    if current[1] < 0:
+                        current = (1, 0)
                         direction = 1
-                    current -= 1
+                    current[1] -= 1
                 else:
-                    if current > 7:
-                        current = self.gameboard[1][7]
+                    if current[1] > 7:
+                        current = (1, 7)
                         direction = 0
-                    current += 1
+                    current[1] += 1
             else:
                 if direction == 0:
-                    if current < 0:
-                        current = self.gameboard[0][0]
+                    if current[1] < 0:
+                        current = (0, 0)
                         direction = 1
-                    current -= 1
+                    current[1] -= 1
                 else:
-                    if current > 7:
-                        current = self.gameboard[0][7]
+                    if current[1] > 7:
+                        current = (0, 7)
                         direction = 0
-                    current += 1
+                    current[1] += 1
 
-        self.gameboard[current] += 1
+            if self.gameboard[current[0]][current[1]] is None:
+                if current[0] == 0:
+                    current[1] += 1
+                else:
+                    current[1] -= 1
+
+            self.gameboard[current[0]][current[1]] += 1
+            print(str(self.gameboard[0]) + "\n" + str(self.gameboard[1]))
+
 
 g1 = Gameboard()
 print(g1)
 g1.sow(3, 1, 1)
+g1.sow(4, 1, 1)
