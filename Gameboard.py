2ecc38bd05fcd6771931e5f640d15eedf6155ed8
cortmans2 <cortmans@potomacschool.org>
2022-10-14 08:25:39 -0400

    commit 2

--- Gameboard.py
+++ Gameboard.py
@@ -1,6 +1,49 @@
+import numpy
+
+
 class Gameboard:
 
-    def __init__(self, gameboard):
+    def __init__(self):
 
-        self.gameboard = gameboard
+        self.gameboard = [[0, 4, 4, 4, 4, 4, 4, None],
+                         [None, 4, 4, 4, 4, 4, 4, 0]]
 
+    def __str__(self):
+
+        return str(self.gameboard[0]) + "\n" + str(self.gameboard[1])
+
+    def sow(self, index, direction, player):
+
+        current = count = self.gameboard[player][index]
+        self.gameboard[player][index] = 0
+
+        for i in range(count):
+
+            if player == 0:
+                if direction == 0:
+                    if current < 0:
+                        current = self.gameboard[1][0]
+                        direction = 1
+                    current -= 1
+                else:
+                    if current > 7:
+                        current = self.gameboard[1][7]
+                        direction = 0
+                    current += 1
+            else:
+                if direction == 0:
+                    if current < 0:
+                        current = self.gameboard[0][0]
+                        direction = 1
+                    current -= 1
+                else:
+                    if current > 7:
+                        current = self.gameboard[0][7]
+                        direction = 0
+                    current += 1
+
+        self.gameboard[current] += 1
+
+g1 = Gameboard()
+print(g1)
+g1.sow(3, 1, 1)
