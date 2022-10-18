b727c2dfc901830341e7af3bc264d825fab9783c
BHalpert <bhalpert@potomacschool.org>
2022-10-18 08:36:20 -0400

    updates to Game Class, fixing the instance variables, constructors and adding variables to Move() in the startTurn method

--- Game.py
+++ Game.py
@@ -4,14 +4,11 @@ import Player
 
 
 class Game:
-    def __init__(self, playerToMove, board, player0, player1):
+    def __init__(self, playerToMove, player0, player1):
         self.playerToMove = playerToMove
-        self.board = board
-        self.player0 = player0
-        self.player1 = player1
-        self.Gameboard = Gameboard()
-        self.player0 = Player()
-        self.player1 = Player()
+        self.gameboard = Gameboard()
+        self.player0 = Player(playerName, 0, gameboard)
+        self.player1 = Player(playerName, 1, gameboard)
 
 
     def getPlayerToMove(self):
@@ -26,6 +23,6 @@ class Game:
     def getPlayer1(self):
         return self.player1
 
-    def startTurn():  # This is a method
+    def startTurn(self):
         Move(index, direction, playerToMove)
         IsGameOver()
