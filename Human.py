469647d47c4914d1557dda5645f0cb1e892a042c
BHalpert <bhalpert@potomacschool.org>
2022-10-26 08:52:59 -0400

    Added comments

--- Human.py
+++ Human.py
@@ -1,18 +1,19 @@
 from Player import *
 class Human(Player):
 
-    def __init__(self, playerName, playerNum, gameboard): # just calls the super constructor
+    def __init__(self, playerName, playerNum, gameboard): # calls the super constructor
         super().__init__(playerName, playerNum, gameboard)
 
-    def Move(self):
+    def Move(self): # Gets which pit to move and which direction to move from player
+                    # and then calls sow(in Gameboard) which acually moves them
         # Index Input
 
         indexToMove = 0
         direction = 0
 
-        try:
+        try:  # If invalid input, calls method again
             indexToMove = 0
-            while int(indexToMove) < 1 or int(indexToMove) > 6:
+            while int(indexToMove) < 1 or int(indexToMove) > 6:  # only runs if indexToMove is invalid
                 indexToMove = (input("<Player" + ("1" if self.num == 0 else "2") + "> Index To Move (1 - 6) : "))
         except:
             print("Invalid Input. Index to move must be in the range of 1 - 6")
@@ -20,14 +21,15 @@ class Human(Player):
             return
 
         if (self.gameboardReference.gameboard[int(self.num)][int(indexToMove)] == 0):
+            # Checks if pit selected has seeds in it
             print("Invalid Input. You must select a pit with seeds in it")
             self.Move()
             return
 
         # Direction Input
-        try:
+        try:   # If invalid input, calls method again
             direction = 0
-            while int(direction) != 1 and int(direction) != -1:
+            while int(direction) != 1 and int(direction) != -1:  # only runs if direction is invalid
                 direction = (input("<Player" + ("1" if self.num == 0 else "2") + "> Direction (-1 or 1) : "))
         except:
             print("Invalid Input. Direction must be -1 or 1")
